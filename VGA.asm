ba9bab6dfe8c41837f16d714da517790b3dd2bc7
Jack <jack.github@gmail.com>
2016-09-07 21:32:11 -0400

    Updated Screen rendering
    Took out the clear screen function as screen data is now in a text buffer in boot_cyno.
    New function just writes text buffer directly to the screen.

--- VGA.asm
+++ VGA.asm
@@ -1,26 +1,15 @@
-;Sub routines to deal with the VGA Display
-VGA_MEM_START equ 0x00b8000
-VGA_COLS equ 160 ; 80 Columns wide doubled because data is 2 bytes wide
-VGA_ROWS equ 25
+; Write VGA Data to the screen.
+; EAX: Must store starting VGA data value (Typically 0x00b8000 for start of VGA)
+; EBX: Must store the end address of data
+; ECX: Must store pointer (addr of) to first element
+; ALL DATA MUST BE 16 BIT VALUES, AS THIS IS WHAT VGA DATA IS IN.
+write_to_screen:
 
-;Requires that you put the color in the EBX Register in the following format:
-; 0x2f00 is green with white text
-; 0x2000 this is also valid as there is no text being printed
-; but needs to be 16 Bit num.
-clear_screen:
-    xor ecx, ecx ; Set the column counter to 0
-    xor edx, edx ; Set the row counter to 0
-    add ebx, 0x0020 ; 32 is the character for space so this will print a blank space
-.loop:
-    mov eax, VGA_MEM_START ; Move the start of VGA Mem
-    add eax, ecx ; Increment it based on the column
-    mov [eax], ebx ; Move to the VGA Mem the value of the Background
-    add ecx, 2 ; Jump by two because VGA Mem Data is 2 bytes wide
-    cmp ecx, VGA_COLS
-    jl .loop
-    add eax, ecx
-    xor ecx, ecx
-    inc edx
-    cmp edx, VGA_ROWS
-    jl .loop
+.wtsloop:
+    mov edx, [ecx] ; Pull the value at that data addr
+    mov [eax], edx ; Write it to VGA Location
+    add ecx, 2 ; Move data pointer up by 2 because its words not bytes
+    add eax, 2 ; Move VGA data up 2 because its words not bytes
+    cmp ecx, ebx ; Compare Data pointer Position with where it should stop
+    jne .wtsloop ; When we hit the end point, return
     ret
